1576e916fb29e9453e631d8b89d76b53aa2781f8
kh3254215-sudo <kh3254215@gmail.com>
2025-12-17 10:37:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -748,3 +748,8 @@ function shutdown() {
   bare.close();
   process.exit(0);
 }
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
